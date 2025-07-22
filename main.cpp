805ba4f1385da4dc3f528e1fe00584b49e58b2d9
kimyaesol <kimyaesol@gmail.com>
2025-07-22 15:25:07 +0900

    add getSum

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include "gmock/gmock.h"
 
 class Cal {
-public : 
-	// ÀÌ°÷¿¡ ÄÚµå ÀÛ¼º 
-	int getMinus(int a, int b) { return a - b; }
-	int getZegop(int a) {
+public :
+	int getSum(int a, int b) 
+	{
+		return a + b;
+	}
+
+	int getMinus(int a, int b)
+	{ 
+		return a - b;
+	}
+
 	int getDivide(int a, int b) {
 		if (b == 0) return 0;
 		return (a / b);
-  }
+	}
     int getZegop(int a) {
 		return a * a;
 	}
 };
+
+
+TEST(TC, SumTest) {
+	Cal cal;
+	int ret = cal.getSum(10, 20);
+	EXPECT_EQ(30, ret);
+}
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
