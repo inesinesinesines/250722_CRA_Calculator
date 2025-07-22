85fe8ff4b46ac49bdd6a45d3853ca5d38488d6cf
inhae.kang <kang.inhae@gmail.com>
2025-07-22 15:30:54 +0900

    add get divide test

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ class Cal {
 public : 
 	// ÀÌ°÷¿¡ ÄÚµå ÀÛ¼º 
 	int getMinus(int a, int b) { return a - b; }
-	int getZegop(int a) {
 	int getDivide(int a, int b) {
 		if (b == 0) return 0;
 		return (a / b);
-  }
+	}
     int getZegop(int a) {
 		return a * a;
 	}
@@ -16,6 +15,12 @@ public :
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, getDivideTest) {
+	Cal cal;
+	int ret = cal.getDivide(1000, 10);
+	EXPECT_EQ(ret, 1000/10);
 }
 
 int main() {
